f2b8e6921d343a7eaa07fb19609d251e78a38482
Planxnx <planxthanee@gmail.com>
2022-12-15 01:00:33 +0700

    feat: add bytedance/sonic

--- go.mod
+++ go.mod
@@ -13,6 +13,11 @@ require (
 )
 
 require (
+	github.com/bytedance/sonic v1.6.0 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/segmentio/asm v1.1.3 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/sys v0.0.0-20220318055525-2edf467146b5 // indirect
 )
--- json/json_test.go
+++ json/json_test.go
@@ -1,10 +1,15 @@
 package json
 
 import (
+	"bytes"
+	"crypto/rand"
+	"encoding/gob"
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/Planxnx/how-fast/utils"
+	"github.com/bytedance/sonic"
 	gojson "github.com/goccy/go-json"
 	segmentiojson "github.com/segmentio/encoding/json"
 	"github.com/wI2L/jettison"
@@ -24,10 +29,15 @@ var benchmarks = []jsonBenchmark{
 		Unmarshal: json.Unmarshal,
 	},
 	{
-		Name:      "goccy_go-json",
-		Package:   "github.com/goccy/go-json",
-		Marshal:   gojson.Marshal,
-		Unmarshal: gojson.Unmarshal,
+		Name:      "encoding_gob",
+		Package:   "encoding/gob",
+		Marshal:   gobMarshal,
+		Unmarshal: gobUnmarshal,
+	},
+	{
+		Name:    "goccy_go-json",
+		Package: "github.com/goccy/go-json",
+		Marshal: gojson.Marshal,
 	},
 	{
 		Name:    "jettison",
@@ -40,11 +50,20 @@ var benchmarks = []jsonBenchmark{
 		Marshal:   segmentiojson.Marshal,
 		Unmarshal: segmentiojson.Unmarshal,
 	},
+	{
+		Name:      "sonic",
+		Package:   "github.com/bytedance/sonic",
+		Marshal:   sonic.Marshal,
+		Unmarshal: sonic.Unmarshal,
+	},
 }
 
 func BenchmarkEncoder(b *testing.B) {
 	smallPayload := NewSmallPayload()
 	largePayload := NewLargePayload()
+	longPayload := NewMapPayload(1000, 1)
+	smallDataPayload := NewMapPayload(8, 1)
+	bigDataPayload := NewMapPayload(8, 2*1024*1024)
 
 	for _, bench := range benchmarks {
 		if bench.Marshal == nil {
@@ -61,6 +80,27 @@ func BenchmarkEncoder(b *testing.B) {
 			b.Run(utils.FieldName("size", "large"), func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
 					if _, err := bench.Marshal(largePayload); err != nil {
+						b.Error(err)
+					}
+				}
+			})
+			b.Run(utils.FieldName("size", "long"), func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					if _, err := bench.Marshal(longPayload); err != nil {
+						b.Error(err)
+					}
+				}
+			})
+			b.Run(utils.FieldName("size", "small_data"), func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					if _, err := bench.Marshal(smallDataPayload); err != nil {
+						b.Error(err)
+					}
+				}
+			})
+			b.Run(utils.FieldName("size", "big_data"), func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					if _, err := bench.Marshal(bigDataPayload); err != nil {
 						b.Error(err)
 					}
 				}
@@ -100,4 +140,40 @@ type jsonBenchmark struct {
 	Package   string
 	Marshal   func(v interface{}) ([]byte, error)
 	Unmarshal func(data []byte, v interface{}) error
+}
+
+func gobMarshal(v interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(v)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func gobUnmarshal(b []byte, result interface{}) error {
+	buf := bytes.NewBuffer(b)
+	enc := gob.NewDecoder(buf)
+
+	err := enc.Decode(result)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewMapPayload(n int, valueSizeBytes ...int) map[string][]byte {
+	size := 8
+	if len(valueSizeBytes) > 0 {
+		size = valueSizeBytes[0]
+	}
+	m := make(map[string][]byte, n)
+	for i := 0; i < n; i++ {
+		v := make([]byte, size)
+		_, _ = rand.Read(v)
+		m[fmt.Sprint(i)] = v
+	}
+	return m
 }
